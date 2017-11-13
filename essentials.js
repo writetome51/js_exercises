c63c078c0d656b301b6aeb05c37fb6e3d3c2d986
Steve Thompson <writetome51@gmail.com>
2017-11-13 11:00:29 -0700

    new commit

--- essentials.js
+++ essentials.js
@@ -13,3 +13,23 @@ function callAsMethodOfTheObject(obj, functionToCall){
     obj.functionToCall = functionToCall;
     obj.functionToCall();
 }
+
+// Intended to use as method on array:
+Array.prototype.removeInnerItem =  function(item){
+    var index = this.indexOf(item);
+    this.splice(index, 1);
+    return this;
+};
+
+Array.prototype.removeAdjacentItems = function(items){
+	var index = this.indexOf(items[0]);
+	this.splice(index, items.length);
+	return this;
+};
+
+Array.prototype.removeNonAdjacentItems = function(items){
+    for (var i=0; i<items.length;  ++i){
+        this.removeInnerItem(items[i]);
+    }
+    return this;
+};
